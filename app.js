7e346fc3eb77f6c3f4d532eb72b7ea8acdc1e505
dev-rakibul1 <fl.rakibul@gmail.com>
2022-07-14 16:27:55 +0600

    javascript dom porject 01

---
+++ app.js
@@ -0,0 +1,12 @@
+// Javascript DOM
+const myImages = document.getElementById("myImages");
+
+// light on
+function changeData() {
+  document.getElementById("myImages").src = "./images/light on.png";
+}
+
+// light off
+function changeData2() {
+  document.getElementById("myImages").src = "./images/light off.png";
+}
